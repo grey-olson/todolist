e41f234aed2674cd30234a04f37fc7e23d77f0cc
Lindsey Olson <l.olson.swift@gmail.com>
2023-08-10 22:33:25 -0400

    Initial Set Up

---
+++ todolist/todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Lindsey Olson on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
